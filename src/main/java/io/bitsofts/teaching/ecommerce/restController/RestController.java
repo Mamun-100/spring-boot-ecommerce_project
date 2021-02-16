2b2b8ec52fae71d01cba3e31808bd80f72e1b7da
Maman-100 <rashid202412@gmail.com>
2021-02-16 19:25:31 +0600

    RestController Edit

--- src/main/java/io/bitsofts/teaching/ecommerce/restController/RestController.java
+++ src/main/java/io/bitsofts/teaching/ecommerce/restController/RestController.java
@@ -16,6 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 /**
@@ -31,17 +34,15 @@ public class RestController {
    @Autowired
    UserRepository ur;
    
-   @GetMapping(path = "/api/products")
-   public ResponseEntity<List<Product>> getAllProdut(){
-       
-       return new ResponseEntity<>(pr.findAll(),HttpStatus.OK);
-   
-   }
-   
-    @GetMapping(path="/api/user")
-   public ResponseEntity<Iterable<User>> getAllUser(){
-   
-       return  new ResponseEntity<>(ur.findAll(),HttpStatus.OK);
-   }
+
+  @RequestMapping(method = RequestMethod.GET, value = "/api/products")
+    public ResponseEntity<List<Product>> getProducts(){
+        return new ResponseEntity<>(pr.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/api/product")
+    public ResponseEntity<Product> getproductById(@RequestParam(name = "id") int id){
+        return new ResponseEntity<>(pr.findById(id), HttpStatus.OK);
+    }
 }
 
